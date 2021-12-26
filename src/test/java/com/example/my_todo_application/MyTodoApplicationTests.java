e7352fec532a4c667838b4e67f8a8239a9ebf6d8
yasu <yasusi0117@live.jp>
2021-12-26 19:56:14 +0900

    init

---
+++ src/test/java/com/example/my_todo_application/MyTodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.my_todo_application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyTodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
