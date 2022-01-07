9d90a71f1f8fc0ad4c59900099621b2b2b3b6de7
yasu <yasusi0117@live.jp>
2022-01-07 17:24:15 +0900

    UserRegisterFormクラスの作成

---
+++ src/main/java/com/example/my_todo_application/task_management/controller/form/UserRegisterForm.java
@@ -0,0 +1,24 @@
+package com.example.my_todo_application.task_management.controller.form;
+
+import lombok.Data;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Data
+public class UserRegisterForm {
+
+    /** 名前 */
+    @NotBlank
+    private String firstName;
+    /** 苗字 */
+    @NotBlank
+    private String lastName;
+    /** メールアドレス */
+    @Email
+    private String mailAddress;
+    /** パスワード */
+    @Size(min = 8)
+    private String password;
+}
