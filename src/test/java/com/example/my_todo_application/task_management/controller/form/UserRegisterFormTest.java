996a786b40e2e403149c8d1e57d0baccf7873b94
yasu <yasusi0117@live.jp>
2022-01-07 17:21:15 +0900

    mailAddress・passwordフィールドの異常系のテスト項目追加

--- src/test/java/com/example/my_todo_application/task_management/controller/form/UserRegisterFormTest.java
+++ src/test/java/com/example/my_todo_application/task_management/controller/form/UserRegisterFormTest.java
@@ -3,6 +3,8 @@ package com.example.my_todo_application.task_management.controller.form;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.validation.BindException;
@@ -91,6 +93,74 @@ class UserRegisterFormTest {
 
             assertTrue(bindingResult.hasFieldErrors("lastName"));
             System.out.println(bindingResult.getFieldError("lastName").getDefaultMessage());
+        }
+    }
+
+    @DisplayName("mailAddressのテスト")
+    @Nested
+    class MailAddress {
+
+        @ParameterizedTest
+        @CsvSource({"mail, test, test@, "})
+        @DisplayName("mailAddressがメール形式でない場合エラーになる")
+        void noneMailAddress(String mailAddress) {
+            // setup
+            UserRegisterForm form = testTaskRegisterForm();
+            form.setMailAddress(mailAddress);
+            BindingResult bindingResult = new BindException(form, "userRegisterForm");
+
+            validator.validate(form, bindingResult);
+
+            assertTrue(bindingResult.hasFieldErrors("mailAddress"));
+            System.out.println(bindingResult.getFieldError("mailAddress").getDefaultMessage());
+        }
+
+        @Test
+        @DisplayName("mailAddressがnullの場合エラーになる")
+        void mailAddressIsNull() {
+            // setup
+            UserRegisterForm form = testTaskRegisterForm();
+            form.setMailAddress(null);
+            BindingResult bindingResult = new BindException(form, "userRegisterForm");
+
+            validator.validate(form, bindingResult);
+
+            assertTrue(bindingResult.hasFieldErrors("mailAddress"));
+            System.out.println(bindingResult.getFieldError("mailAddress").getDefaultMessage());
+        }
+    }
+
+    @DisplayName("passwordのテスト")
+    @Nested
+    class Password {
+
+        @ParameterizedTest
+        @CsvSource({"pass, test123, tes, "})
+        @DisplayName("passwordが8文字未満の場合エラーになる")
+        void nonePassword(String password) {
+            // setup
+            UserRegisterForm form = testTaskRegisterForm();
+            form.setPassword(password);
+            BindingResult bindingResult = new BindException(form, "userRegisterForm");
+
+            validator.validate(form, bindingResult);
+
+            assertTrue(bindingResult.hasFieldErrors("password"));
+            System.out.println(bindingResult.getFieldError("password").getDefaultMessage());
+        }
+
+        @Test
+        @DisplayName("passwordがnullの場合エラーになる")
+        void passwordIsNull() {
+            // setup
+            UserRegisterForm form = testTaskRegisterForm();
+            form.setPassword(null);
+            BindingResult bindingResult = new BindException(form, "userRegisterForm");
+
+            validator.validate(form, bindingResult);
+
+            assertTrue(bindingResult.hasFieldErrors("password"));
+            System.out.println(bindingResult.getFieldError("password").getDefaultMessage());
         }
     }
 
