e59f07045ee6ea8f9e4f63a91b73c3b1e0ad09df
yasu <yasusi0117@live.jp>
2022-01-13 01:45:39 +0900

    テスト用のdata.sql schema.sqlファイルの追加

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into role(role_name) values('ROLE_GENERAL');
+
+insert into app_user(first_name, last_name, mail_address, password, role_id) values('太郎', '山田', 'taro@xxx.xx.xx', 'password', 1);
