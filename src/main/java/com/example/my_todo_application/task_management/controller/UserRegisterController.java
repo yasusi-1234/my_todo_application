be2e62decd335e0d8bbde79a0b9e7fd1ce15e188
yasu <yasusi0117@live.jp>
2022-01-13 22:43:25 +0900

    UserRegisterControllerクラスの作成(ユーザー新規登録用)
    ページの入り口のgetUserRegisterメソッド生成
    
    登録リクエスト用のpostUserRegisterメソッドの生成

--- src/main/java/com/example/my_todo_application/task_management/controller/UserRegisterController.java
+++ src/main/java/com/example/my_todo_application/task_management/controller/UserRegisterController.java
@@ -1,4 +1,22 @@
 package com.example.my_todo_application.task_management.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("user")
 public class UserRegisterController {
+
+    @GetMapping("register")
+    public String getUserRegister(){
+
+        return "user/user-register";
+    }
+
+    @PostMapping("register")
+    public String postUserRegister(){
+        return "user/user-register";
+    }
 }
