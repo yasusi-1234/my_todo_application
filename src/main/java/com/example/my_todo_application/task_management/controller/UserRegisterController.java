3b9cd8371e3a504015614aabf37d01cb3d391da0
yasu <yasusi0117@live.jp>
2022-01-13 22:54:59 +0900

    ユーザー登録ページにアクセスした際のGetページのメソッドの引数設定。
    登録リクエストのPostメソッドの引数を設定。
    
    userRegisterServiceクラスのDI注入

--- src/main/java/com/example/my_todo_application/task_management/controller/UserRegisterController.java
+++ src/main/java/com/example/my_todo_application/task_management/controller/UserRegisterController.java
@@ -1,22 +1,39 @@
 package com.example.my_todo_application.task_management.controller;
 
+import com.example.my_todo_application.task_management.controller.form.UserRegisterForm;
+import com.example.my_todo_application.task_management.domain.service.UserRegisterService;
+import lombok.RequiredArgsConstructor;
+import org.apache.catalina.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @Controller
 @RequestMapping("user")
+@RequiredArgsConstructor
 public class UserRegisterController {
 
-    @GetMapping("register")
-    public String getUserRegister(){
+    UserRegisterService userRegisterService;
 
+    @GetMapping("register")
+    public String getUserRegister(
+            @ModelAttribute("userRegisterForm")UserRegisterForm userRegisterForm){
         return "user/user-register";
     }
 
     @PostMapping("register")
-    public String postUserRegister(){
+    public String postUserRegister(
+            @Validated @ModelAttribute("userRegisterForm")UserRegisterForm userRegisterForm,
+            BindingResult bindingResult,
+            UriComponentsBuilder uriBuilder,
+            RedirectAttributes redirectAttributes){
         return "user/user-register";
     }
 }
