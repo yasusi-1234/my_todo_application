4a0abf8e6289a462f523cd23bb8f0ec95cc85c1f
yasu <yasusi0117@live.jp>
2022-01-15 17:55:42 +0900

    セキュリティーの設定用クラスのToDoSecurityクラスの実装

---
+++ src/main/java/com/example/my_todo_application/security/config/ToDoSecurity.java
@@ -0,0 +1,10 @@
+package com.example.my_todo_application.security.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+@Configuration
+public class ToDoSecurity extends WebSecurityConfigurerAdapter {
+}
