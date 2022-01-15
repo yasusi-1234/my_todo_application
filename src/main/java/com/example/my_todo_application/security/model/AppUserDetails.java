d2fd9a76d2cf3201178d867e690c1f26d46c25d8
yasu <yasusi0117@live.jp>
2022-01-15 16:54:49 +0900

    AppUserDetailsの内部実装を記述

--- src/main/java/com/example/my_todo_application/security/model/AppUserDetails.java
+++ src/main/java/com/example/my_todo_application/security/model/AppUserDetails.java
@@ -1,4 +1,70 @@
 package com.example.my_todo_application.security.model;
 
-public class AppUserDetails {
+import com.example.my_todo_application.task_management.domain.model.AppUser;
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+
+public class AppUserDetails implements UserDetails {
+
+    /** 元になる {@link AppUser}クラス */
+    @Getter
+    private final AppUser appUser;
+
+    /** パスワード */
+    private final String password;
+
+    /** ユーザー名(メールアドレス) */
+    private final String userName;
+
+    /** ロール情報 */
+    private final List<GrantedAuthority> authorities;
+
+    public AppUserDetails(AppUser appUser) {
+        this.appUser = appUser;
+        this.password = appUser.getPassword();
+        this.userName = appUser.getMailAddress();
+        this.authorities = AuthorityUtils.createAuthorityList(appUser.getRole().getRoleName());
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return this.authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.userName;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
