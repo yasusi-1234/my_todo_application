a7e735a1a1077c305b8987fd6b2da1109435bd15
yasu <yasusi0117@live.jp>
2022-01-15 13:34:10 +0900

    TaskControllerクラスの生成

--- src/main/java/com/example/my_todo_application/task_management/controller/TaskController.java
+++ src/main/java/com/example/my_todo_application/task_management/controller/TaskController.java
@@ -1,4 +1,17 @@
 package com.example.my_todo_application.task_management.controller;
 
+import com.example.my_todo_application.task_management.domain.service.TaskService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("task")
+@RequiredArgsConstructor
 public class TaskController {
+
+
+    private final TaskService taskService;
+
+
 }
