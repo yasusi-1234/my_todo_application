9984ebb8ad9f6250ea420406dceb08220a319957
yasu <yasusi0117@live.jp>
2022-01-15 13:43:07 +0900

    getTaskHomeメソッドの作成

--- src/main/java/com/example/my_todo_application/task_management/controller/TaskController.java
+++ src/main/java/com/example/my_todo_application/task_management/controller/TaskController.java
@@ -1,8 +1,14 @@
 package com.example.my_todo_application.task_management.controller;
 
+import com.example.my_todo_application.task_management.controller.form.Progress;
+import com.example.my_todo_application.task_management.controller.form.TaskRegisterForm;
+import com.example.my_todo_application.task_management.controller.form.TaskSearchForm;
 import com.example.my_todo_application.task_management.domain.service.TaskService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -13,5 +19,15 @@ public class TaskController {
 
     private final TaskService taskService;
 
+
+    @GetMapping("home")
+    public String getTaskHome(
+            @ModelAttribute("taskSearchForm") TaskSearchForm taskSearchForm,
+            @ModelAttribute("TaskRegisterFrom")TaskRegisterForm taskRegisterForm,
+            Model model){
+        model.addAttribute("progress", Progress.values());
+
+        return "task/task-home";
+    }
 
 }
