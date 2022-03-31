77ab53c09753796e2f7cfab54c3f82b0b00b9a16
yasu <yasusi0117@live.jp>
2022-03-31 18:07:58 +0900

    各Entityのフィールド名を定義したクラスの生成

---
+++ src/main/java/com/example/my_todo_application/task_management/common/EntityNames.java
@@ -0,0 +1,32 @@
+package com.example.my_todo_application.task_management.common;
+
+public final class EntityNames {
+
+    /** Entity AppUser */
+    public static final String APP_USER = "appUser";
+
+    /** userId of Entity AppUser */
+    public static final String APP_USER_ID = "appUserId";
+
+    /** TaskID of Entity Task */
+    public static final String TASK_ID = "taskId";
+
+    /** TaskName of Entity Task */
+    public static final String TASK_NAME = "taskName";
+
+    /** endDate of Entity Task */
+    public static final String END_DATE = "endDate";
+
+    /** startDate of Entity Task */
+    public static final String START_DATE = "startDate";
+
+    /** importance of Entity Task */
+    public static final String IMPORTANCE = "importance";
+
+    /** notice of Entity Task */
+    public static final String NOTICE = "notice";
+
+    /** progress of Entity Task */
+    public static final String PROGRESS = "progress";
+
+}
