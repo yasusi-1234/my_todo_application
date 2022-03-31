7dbf458ad9e5ab38755efc7eda1eef8481b346dc
yasu <yasusi0117@live.jp>
2022-03-31 18:10:52 +0900

    LocalDateTimeだったフィールドをLocalDate型に変更
    DateBetweenValidatorをクラスレベルに追加
    
    ImportanceのフィールドをList型に変更

--- src/main/java/com/example/my_todo_application/task_management/controller/form/TaskSearchForm.java
+++ src/main/java/com/example/my_todo_application/task_management/controller/form/TaskSearchForm.java
@@ -1,24 +1,30 @@
 package com.example.my_todo_application.task_management.controller.form;
 
+import com.example.my_todo_application.task_management.controller.validator.DateBetween;
+import com.example.my_todo_application.task_management.controller.validator.FormAction;
 import com.example.my_todo_application.task_management.domain.model.Importance;
 import lombok.Data;
 import org.springframework.format.annotation.DateTimeFormat;
 
-import java.time.LocalDateTime;
+import java.time.LocalDate;
+import java.util.List;
 
 @Data
+@DateBetween(startField = "fromDate", endField = "toDate", formType = FormAction.READ)
 public class TaskSearchForm {
 
     /** タスク名 */
     private String taskName;
     /** 検索したい開始時刻 */
-    @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
-    private LocalDateTime fromDate;
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDate fromDate;
     /** 検索したい終了時刻 */
-    @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
-    private LocalDateTime toDate;
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDate toDate;
+    /** タスク詳細 */
+    private String detail;
     /** タスク重要度 */
-    private Importance importance;
+    private List<Importance> importanceList;
     /** タスク進捗状況 */
     private Progress progress;
 
